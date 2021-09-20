c2a6c8c36839b511af0703aa2f2d27b53615b440
Bela Géréon <belagereon@gmx.de>
2021-09-20 14:49:58 +0200

    create GameManager class in new package

---
+++ src/Game/GameManager.java
@@ -0,0 +1,17 @@
+package Game;
+
+import GameEngine.AbstractGame;
+import GameEngine.GameContainer;
+import GameEngine.Renderer;
+
+public class GameManager extends AbstractGame {
+    @Override
+    public void update(GameContainer gameContainer, float dt) {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Renderer renderer) {
+
+    }
+}
